4fca4156f21703907c5e52d91cbbfefcad593d5d
TAVICRACK841 <gustavolizama841@gmail.com>
2024-03-13 16:31:48 -0600

    feat: Cree un componebte y se agrego tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.astro'],
+	theme: {
+		extend: {
+			colors: {
+				purple: "#812B8C",
+				magenta: "#BF247A",
+				orange: "#D9731A",
+				purplish: "#2A2359",
+				red: "#BF3939"
+			}
+		},
+	},
+	plugins: [],
+}
